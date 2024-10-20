27a48b088fd2b3b1efb406b3ece5e28c84b7e11e
tachu <>
2024-10-20 17:12:16 +0900

    add: render func

--- src/jsx/hyperscript.ts
+++ src/jsx/hyperscript.ts
@@ -1,8 +1,33 @@
 import { VNode, VNodeProps } from "./vnode.ts";
 
-const STRING = "string";
+/** hypertext + javascript = hyperscript
+ ex) jsx
+ <div id="container">
+    <h1>Hello, JSX!</h1>
+    <p>This is a paragraph.</p>
+  </div>
 
-// hypertext + javascript = hyperscript
+ex) VNode
+{
+  "tag": "div",
+  "props": {
+    "id": "container"
+  },
+  "children": [
+    {
+      "tag": "h1",
+      "props": {},
+      "children": ["Hello, JSX!"]
+    },
+    {
+      "tag": "p",
+      "props": {},
+      "children": ["This is a paragraph."]
+    }
+  ]
+}
+ *
+*/
 function h(
   tag: string,
   props: VNodeProps,
@@ -15,10 +40,19 @@ function h(
   };
 }
 
-// function render(vnode: VNode | string): VNode {
-//   if (typeof vnode === STRING) {
-//     return document.createTextNode(vnode);
-//   }
-//   const tag = document.createElement(vnode.tag);
-//
-// }
+function render(vnode: VNode | string): HTMLElement | Text {
+  if (typeof vnode === "string") {
+    return document.createTextNode(vnode);
+  }
+  const tag = document.createElement(vnode.tag);
+
+  // set attribute to tag
+  // ex) <div class="js-body">
+  const props = vnode.props || {};
+  Object.keys(props).forEach((key) => tag.setAttribute(key, props.key));
+
+  // add children tag to parent tag
+  const children = vnode.children || [];
+  children.forEach((child) => tag.appendChild(render(child)));
+  return tag;
+}
